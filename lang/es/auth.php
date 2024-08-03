d70bf839cfa01156257460bf1ecd0f654308f36b
Eduardo Gonzalez <heyimedd@gmail.com>
2024-08-03 01:30:41 -0600

    Traducción parcial breeze

---
+++ lang/es/auth.php
@@ -0,0 +1,16 @@
+return [
+/*
+|--------------------------------------------------------------------------
+| Líneas de idioma de autenticación
+|--------------------------------------------------------------------------
+|
+| Las siguientes líneas de idioma se utilizan durante la autenticación para
+| varios mensajes que necesitamos mostrar al usuario. Puedes modificar
+| estas líneas de idioma según los requisitos de tu aplicación.
+|
+*/
+
+'failed' => 'Estos datos no coinciden con los que tenemos registrados.',
+'password' => 'La contraseña proporcionada es incorrecta.',
+'throttle' => 'Demasiados intentos de inicio de sesión. Por favor, inténtalo de nuevo en :seconds segundos.',
+];
